74a6ba818de5dcdb8fbe03c04cf57d729c3c0187
lzr2006 <65013694+lzr2006@users.noreply.github.com>
2021-05-05 13:07:57 +0800

    Add files via upload

---
+++ GiveMoney/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Card_Live
+{
+    public class GiveMoney
+    {
+
+        public int give(int day)
+        {
+            int start = 200;
+            Random rd = new Random();
+            int c = rd.Next(1,10);
+            int give = 200 - day*c;
+            return give;
+        }
+    }
+}
