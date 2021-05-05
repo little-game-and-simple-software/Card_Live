03577a0965abd26b72a564a0d1a3ce2ab1a7cde4
lzr2006 <65013694+lzr2006@users.noreply.github.com>
2021-05-05 13:06:23 +0800

    Add files via upload

---
+++ GetRanCard/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Card_Live
+{
+    public class GetRanCard
+    {
+        public int Get()
+        {
+            //1:甲 2:乙 3:丙 4:丁 
+            Random rd = new Random();
+            int temp = rd.Next(1,11);
+            return temp;
+
+        }
+    }
+}
